5bfe9b59b8d593553596a714cd4ed52e6afaea36
venaj <vena.janoch@windowslive.com>
2020-10-23 22:03:22 +0200

    First version
    Base of view and function

---
+++ FarmaVojovice/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FarmaVojovice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Naše farma je lepší než všechny farmy na světě. Máme největšího bejka v republice a ten vám natrhne prdel raz dva";
+            return View();
+        }
+
+        public ActionResult Zrani()
+        {
+            ViewBag.Message = "U nás zraje nejlíp.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Dostihnete nás:";
+
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Offer()
+        {
+            ViewBag.Message = "Naleznete u nás";
+
+            return View();
+        }
+
+
+    }
+}
---
+++ FarmaVojovice/Scripts/StickyScript.js
@@ -0,0 +1,21 @@
+﻿window.onscroll = function () {countOffset()};
+
+var navbar = document.getElementById("myNavbar");
+var sticky = navbar.offsetTop;
+
+function countOffset() {
+    if (window.pageYOffset + 30 >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+function showSmallMenu() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+} 
