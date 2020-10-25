e6fb3c5a253355dce426ea9710c14798c9d3969a
venaj <vena.janoch@windowslive.com>
2020-10-25 16:42:34 +0100

    Offer focus

---
+++ FarmaVojovice/Controllers/OfferController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FarmaVojovice.Controllers
+{
+    public class OfferController : Controller
+    {
+        public ActionResult _OfferTypeFocus()
+        {
+            ViewBag.Message = "Balicek xyz - 1$";
+            return View();
+        }
+    }
+}
